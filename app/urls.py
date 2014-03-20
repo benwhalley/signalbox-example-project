fc8ed3b1d5a8bad45ad47f0355175476d2ff08b3
Ben Whalley <benwhalley@gmail.com>
2014-03-20 02:00:17 +0000

    Work to make django 1.6 compatible. Linked to changes in same branch in signalbox project.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.contrib import admin
@@ -11,7 +11,7 @@ urlpatterns = i18n_patterns('',
         (r'^', include('signalbox.urls')),
         (r'^admin/', include(admin.site.urls)),
         (r'^accounts/', include('registration.backends.simple.urls')),
-        (r'^', include('cms.urls')),
+        # (r'^', include('cms.urls')),
         (r'^reset/password/$', password_reset, {}),
         (r'^robots\.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 )
