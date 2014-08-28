3f5631b3c6379a3124d2a1dcbf5ed0339b2f87a5
Ben Whalley <benwhalley@gmail.com>
2014-08-28 14:11:56 +0100

    added redirect to studies

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +9,9 @@ admin.autodiscover()
 from django.contrib.auth.views import password_reset
 
 urlpatterns = i18n_patterns('',
+        url(r'^$', RedirectView.as_view(url='/studies/', permanent=False), name='index'),
         (r'^', include('signalbox.urls')),
+
         (r'^admin/', include(admin.site.urls)),
         (r'^accounts/', include('registration.backends.simple.urls')),
         # (r'^', include('cms.urls')),
