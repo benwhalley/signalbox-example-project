cf956d0f4bea8c8c9fda8f8972d695dcfdc3ee88
Ben Whalley <benwhalley@gmail.com>
2014-09-02 15:47:09 +0100

    wsgi

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=$PORT app.wsgi:application
+web: waitress-serve --expose-tracebacks --port=$PORT app.wsgi:application
--- app/wsgi.py
+++ app/wsgi.py
@@ -5,14 +5,12 @@ import django
 django.setup()
 
 from django.core.wsgi import get_wsgi_application
-import django.core.handlers.wsgi
 from dj_static import Cling
+
 # we have to load models or we end up in importerror hell when gunicorn
 # tries to import urls... this is borrowed from what runserver does.
 from django.db.models.loading import get_models
 get_models()
 
 # serve static media via gunicorn see dj-static
-application = Cling(django.core.handlers.wsgi.WSGIHandler())
-
 application = Cling(get_wsgi_application())
