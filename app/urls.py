acfc857db99e3e3fb5a1ee938ab17c89a30fdfe5
Ben Whalley <benwhalley@gmail.com>
2015-11-17 11:58:54 +0000

    pep8

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,11 @@ admin.autodiscover()
 from django.contrib.auth.views import password_reset
 
 urlpatterns = i18n_patterns('',
-        url(r'^$', RedirectView.as_view(url='/studies/', permanent=False), name='index'),
-        (r'^', include('signalbox.urls')),
-        (r'^admin/', include(admin.site.urls)),
-        (r'^accounts/', include('registration.backends.simple.urls')),
-        (r'^reset/password/$', password_reset, {}),
-        (r'^robots\.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-        (r'^complete/setup/', TemplateView.as_view(template_name='complete.html')),
+    url(r'^$', RedirectView.as_view(url='/studies/', permanent=False), name='index'),
+    (r'^', include('signalbox.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('registration.backends.simple.urls')),
+    (r'^reset/password/$', password_reset, {}),
+    (r'^robots\.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    (r'^complete/setup/', TemplateView.as_view(template_name='complete.html')),
 )
